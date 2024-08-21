9432f8c83205929be2b3a2cfe63fdefaeea45ec7
Gustavo <88571393+GustaDNS@users.noreply.github.com>
2024-08-21 15:31:58 +0100

    Add almost everything (the initial part)

---
+++ include/so_long.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gudaniel <gudaniel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/01 12:42:47 by gudaniel          #+#    #+#             */
+/*   Updated: 2024/08/21 15:23:30 by gudaniel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "../libraries/minilibx-linux/mlx.h"
+# include "../libraries/libft/libft.h"
+# include "../libraries/libft/get_next_line/get_next_line.h"
+# include "../libraries/libft/ft_printf/ft_printf.h"
+# include <X11/keysym.h>
+# include <X11/X.h>
+# include <stdbool.h>
+
+//--------------------MAP----------------------//
+
+//------------------HANDLE--------------------//
+
+#endif
